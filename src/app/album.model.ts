2bfc600ac68990f5021b509ddd114217c8f1fe91
Ryan mcalpin and jahan walsh <ryan+jahan@>
2017-05-10 10:06:58 -0700

    add initial files and album model.

---
+++ src/app/album.model.ts
@@ -0,0 +1,3 @@
+export class Album {
+  constructor(public name: string, public manager: string, public description: string, public genre: string,  public reward: string, public currentAmount: number, public goal: number ){}
+}
